b42061cbc086beac5a0dcaab350df08636c0eaf1
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-03-17 12:21:35 +0100

    Eslint autofix

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,10 @@ module.exports = {
     ],
   },
   chainWebpack: (config) => {
+    //Eslint autofix
+    config.module.rule('eslint').use('eslint-loader').options({
+      fix: true
+    })
     //Pug loader
     config.module
       .rule("pug")
@@ -21,7 +25,7 @@ module.exports = {
       locale: "en",
       fallbackLocale: "en",
       localeDir: "locales",
-      enableInSFC: false,
-    },
-  },
+      enableInSFC: false
+    }
+  }
 };
